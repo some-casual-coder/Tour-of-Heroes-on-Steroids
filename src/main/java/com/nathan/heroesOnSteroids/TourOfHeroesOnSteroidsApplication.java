d38218a8e105f16270aafc1607f90fa142a246eb
some-casual-coder <nathanngethe@gmail.com>
2021-02-01 12:20:53 +0300

    Initializes the project with getting started content

---
+++ src/main/java/com/nathan/heroesOnSteroids/TourOfHeroesOnSteroidsApplication.java
@@ -0,0 +1,13 @@
+package com.nathan.heroesOnSteroids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourOfHeroesOnSteroidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourOfHeroesOnSteroidsApplication.class, args);
+	}
+
+}
